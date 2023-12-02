ad701f3f558f84b12c5f0e353124178a924f45c7
tomkos5 <149247065+tomkos5@users.noreply.github.com>
2023-12-02 12:02:49 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(`<html><head><title>Rozwiazanie zadania PZC 2.1</title></head><body><p>${new Date().toISOString()}</p></body></html>`);
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>404 Not Found</h1>');
-    }
 });
 
 const PORT = process.env.PORT || 3000;
